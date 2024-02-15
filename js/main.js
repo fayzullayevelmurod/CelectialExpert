f885e682b73b8f86528a82c0761dc6136bf6d2a5
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-02-16 00:47:33 +0500

    home desktop in progress

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 let header_burger = document.querySelector('.header_burger');
 let mobile_menu = document.querySelector('.mobile_menu');
+let hidden = document.querySelector('.hidden');
 
 header_burger.addEventListener('click', () => {
     header_burger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
+    hidden.classList.toggle('active');
 })
 
 let mobile_dropdown_btn = document.querySelector('.mobile_dropdown_btn');
