12611fae3527c8e44793088f78fae397857ec92f
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-02-16 13:08:47 +0500

    services the end

--- js/main.js
+++ js/main.js
@@ -16,3 +16,38 @@ let mobile_dropdown_link = document.querySelector('.mobile_dropdown_link');
 mobile_dropdown_btn.addEventListener('click', () => {
     mobile_dropdown_link.classList.toggle('active')
 })
+
+
+// services
+var init = false;
+var swiper;
+function servicesSwiper() {
+    if (window.innerWidth <= 743) {
+        if (!init) {
+            init = true;
+            swiper = new Swiper(".servicesSwiper", {
+                slidesPerView: 1.50,
+                spaceBetween: 10,
+                breakpoints: {
+                    743: {
+                        slidesPerView: 1.50,
+                    }
+                },
+                pagination: {
+                    el: ".services-pagination",
+                },
+            });
+        }
+    } else if (init) {
+        swiper.destroy();
+        init = false;
+    }
+}
+
+servicesSwiper();
+
+window.addEventListener("resize", () => {
+    servicesSwiper();
+});
+
+// services
