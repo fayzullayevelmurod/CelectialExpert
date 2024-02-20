3d43190504182637ff2ad7cac244a2f01dc558e7
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-02-20 18:30:04 +0500

    error page the end

--- js/main.js
+++ js/main.js
@@ -2,12 +2,13 @@ let header_burger = document.querySelector('.header_burger');
 let mobile_menu = document.querySelector('.mobile_menu');
 let hidden = document.querySelector('.hidden');
 let header = document.querySelector('header');
+let header_blue = document.querySelector('.header_blue');
 
 header_burger.addEventListener('click', () => {
     header_burger.classList.toggle('active');
     mobile_menu.classList.toggle('active');
     hidden.classList.toggle('active');
-    header.classList.toggle('header_blue');
+    header.classList.toggle('active');
 })
 
 let mobile_dropdown_btn = document.querySelector('.mobile_dropdown_btn');
